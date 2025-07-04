1e7de4addca3f674350d1085697864c85f7a2645
LINUXexpert.org <51408202+LINUXexpert-org@users.noreply.github.com>
2025-07-04 00:29:18 -0700

    Update security_audit.py

--- security_audit.py
+++ security_audit.py
@@ -2,6 +2,20 @@
 """
 security_audit.py - Security Audit: Check for common security issues (permissions, open ports).
 
+Copyright (C) 2025 LINUXexpert.org
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the 
+Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+for more details.
+
+You should have received a copy of the GNU General Public License along 
+with this program. If not, see <https://www.gnu.org/licenses/>.
+
 Usage:
     security_audit.py    (no arguments)
 
