ba9ca0bf1de69fa2854975e8a4b018b5f7560f3e
LINUXexpert.org <51408202+LINUXexpert-org@users.noreply.github.com>
2025-07-04 00:28:34 -0700

    Update network_info.py

--- network_info.py
+++ network_info.py
@@ -2,6 +2,20 @@
 """
 network_info.py - Network & Firewall Info: Show network interfaces, routes, open ports, and firewall rules.
 
+Copyright (C) 2025 LINUXexpert.org
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the 
+Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+for more details.
+
+You should have received a copy of the GNU General Public License along 
+with this program. If not, see <https://www.gnu.org/licenses/>.
+
 Usage:
     network_info.py    (no arguments)
 
