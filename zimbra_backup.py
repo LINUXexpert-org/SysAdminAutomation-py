0a831d6cdfcf877a8a43f354325df0f253cf0b4e
LINUXexpert.org <51408202+LINUXexpert-org@users.noreply.github.com>
2025-07-04 00:30:12 -0700

    Update zimbra_backup.py

--- zimbra_backup.py
+++ zimbra_backup.py
@@ -2,6 +2,20 @@
 """
 zimbra_backup.py - Backup a Zimbra mailbox to a TGZ file.
 
+Copyright (C) 2025 LINUXexpert.org
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the 
+Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+for more details.
+
+You should have received a copy of the GNU General Public License along 
+with this program. If not, see <https://www.gnu.org/licenses/>.
+
 Usage:
     zimbra_backup.py    (interactive, prompts for email and backup directory)
 
