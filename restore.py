e97518d66344ec4189473fb6518aa7335a5a1188
LINUXexpert.org <51408202+LINUXexpert-org@users.noreply.github.com>
2025-07-04 00:28:57 -0700

    Update restore.py

--- restore.py
+++ restore.py
@@ -2,6 +2,20 @@
 """
 restore.py - Restore Utility: Restore files from backup archives.
 
+Copyright (C) 2025 LINUXexpert.org
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the 
+Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+for more details.
+
+You should have received a copy of the GNU General Public License along 
+with this program. If not, see <https://www.gnu.org/licenses/>.
+
 Usage:
     restore.py <backup_archive.tar.gz> [target_directory]
 
