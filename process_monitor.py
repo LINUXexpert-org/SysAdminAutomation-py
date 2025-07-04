20555f7d68aaf3137dfcd900c4b2acc3f8bed45f
LINUXexpert.org <51408202+LINUXexpert-org@users.noreply.github.com>
2025-07-04 00:28:45 -0700

    Update process_monitor.py

--- process_monitor.py
+++ process_monitor.py
@@ -2,6 +2,20 @@
 """
 process_monitor.py - Process Management: List top processes and allow termination by name or PID.
 
+Copyright (C) 2025 LINUXexpert.org
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the 
+Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+for more details.
+
+You should have received a copy of the GNU General Public License along 
+with this program. If not, see <https://www.gnu.org/licenses/>.
+
 Usage:
     process_monitor.py            (shows top CPU & memory processes)
     process_monitor.py kill <process_name|PID>
